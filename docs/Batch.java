360d382d2fa7e93c14daeb4a591c6a51dce628f4
Aliabbas Merchant <merchantaliabbas@gmail.com>
2019-07-21 10:13:17 +0530

    Added Example of Batch

---
+++ docs/Batch.java
@@ -0,0 +1,40 @@
+import com.spaceuptech.space_api.API;
+import com.spaceuptech.space_api.sql.Batch;
+import com.spaceuptech.space_api.sql.SQL;
+import com.spaceuptech.space_api.utils.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Batch {
+
+    public static void main(String[] args) throws Exception {
+        API api = new API("books-app", "localhost", 8081);
+        SQL db = api.MySQL();
+        Batch batch = db.beginBatch();
+        Map<String, String> document = new HashMap<>();
+        document.put("name", "aBook");
+        batch.add(db.insert("books").doc(document));
+        HashMap<String, Object> set = new HashMap<>();
+        set.put("name", "Book1");
+        batch.add(db.update("books").where(new Cond("id", "==", 1)).set(set));
+        batch.add(db.delete("books").where(new Cond("id", "==", 1)));
+        batch.apply(new Utils.ResponseListener() {
+            @Override
+            public void onResponse(int statusCode, Response response) {
+                if (statusCode == 200) {
+                    System.out.println("Success");
+                } else {
+                    System.out.println(response.getError());
+                }
+            }
+
+            @Override
+            public void onError(Exception e) {
+                System.out.println(e.getMessage());
+            }
+        });
+        while (true) {
+        }
+    }
+}
